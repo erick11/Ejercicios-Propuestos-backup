3b2112a437d2ac910b747e58c08609a7358ec30b
pc <pc@DESKTOP-5DKBP25>
2015-10-19 23:05:28 -0500

    - Ejercicio de condicionales facil

---
+++ Ejercicos-Propuestos/src/c05_condicionales/Ejercicio01.java
@@ -0,0 +1,16 @@
+package c05_condicionales;
+
+public class Ejercicio01 {
+
+        /*
+         * 01) Determinar las raíces de una ecuación de segundo grado del tipo:
+         * 
+         * Ax2 + Bx + C = 0. 
+         * Considerar el caso en que las raíces fueran
+         * imaginarias.
+         */
+        
+        public static void main(String[] args) {
+
+        }
+}
