e54dbb51daab99da5da5a139f4a98a7efb898d9f
Erick Eduardo <erick.eduardo.ee7@gmail.com>
2015-10-21 11:49:25 -0500

    - Arreglando formato de c05_condicionales Ejercicio02

--- Ejercicos-Propuestos/src/c05_condicionales/Ejercicio02.java
+++ Ejercicos-Propuestos/src/c05_condicionales/Ejercicio02.java
@@ -8,7 +8,8 @@ public class Ejercicio02 {
          /* Determinar el menor valor de 5 números, indicando además a qué orden
          * pertenece. Considere que el menor valor puede repetirse. Por Ejm. Si los
          * números ingresados fueran: 14, 19, 14, 16, 15. Los resultados deben ser:
-         * El menor valor ingresado fue 14 y corresponden al primer número, tercer número. */
+         * El menor valor ingresado fue 14 y corresponden al primer número, tercer número. 
+         */
 
         public static void main(String[] args) {
                 // Declaracion de variables
@@ -26,13 +27,13 @@ public class Ejercicio02 {
 
                 // Proceso de calculo
                   menor = arNumeros[0];
-      for (int i = 0; i < arNumeros.length; i++) {
-         if (menor > arNumeros[i])
-         menor = arNumeros[i];
-      }
+                  for (int i = 0; i < arNumeros.length; i++) {
+                	  if (menor > arNumeros[i])
+                		  menor = arNumeros[i];
+                  }
 
           
-     // Salida de resultados
+                // Salida de resultados
                 System.out.println("Numero menor: " + menor);
         }
 }
